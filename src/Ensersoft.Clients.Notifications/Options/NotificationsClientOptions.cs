6a384a5b18e7f06ba0ebb394b93787cdb80b896e
rainfellow <yariktoruk@gmail.com>
2023-08-30 15:17:05 +0300

    Added Injector to Predictions/Notifications

---
+++ src/Ensersoft.Clients.Notifications/Options/NotificationsClientOptions.cs
@@ -0,0 +1,7 @@
+using Ensersoft.Clients.Common.Options;
+
+namespace Ensersoft.Clients.Notifications.Options;
+
+public class NotificationsClientOptions : InternalClientOptionsBase
+{
+}
---
+++ src/Ensersoft.Clients.Predictions/Options/PredictionsClientOptions.cs
@@ -0,0 +1,7 @@
+using Ensersoft.Clients.Common.Options;
+
+namespace Ensersoft.Clients.Predictions.Options;
+
+public class PredictionsClientOptions : InternalClientOptionsBase
+{
+}
